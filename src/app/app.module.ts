165e5bd5b6c9afc1f4fcbdefda3dabcc8c633289
Giovanny Betancourt G <geobg711@hotmail.com>
2018-01-30 18:06:46 -0500

    C1 - Dev - Basic routing implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,34 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 
+// === Routing Module ===
+import {AppRoutingModule, routeableComponents} from './app-routing';
+
 //  === Components ===
 import { AppComponent } from './app.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PassTrainingsComponent } from './training/pass-trainings/pass-trainings.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PassTrainingsComponent,
+    //  Routeable Components
+    routeableComponents
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     // Angular Material Module,
-    MaterialModule
+    MaterialModule,
+    //  Routing
+    AppRoutingModule
 
   ],
   providers: [],
